000801c04ed07e05f4ea3daa2430e5f8335c28df
qfstudy <qifeistudy@gmail.com>
2018-05-18 15:45:36 +0800

    new

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -2,10 +2,22 @@
     let view = {
         el: '#app',
         template: `
-         <audio autoply src={{url}}></sudio>
+         <audio src={{url}}></audio>
+         <div>
+            <button class="play">播放</button>
+            <button class="pause">暂停</button>
+         </div>
         `,
         render(data){
             $(this.el).html(this.template.replace('{{url}}',data.url))
+        },
+        play(){
+            let audio=$(this.el).find('audio')[0]
+            audio.play()
+        },
+        pause(){
+            let audio=$(this.el).find('audio')[0]
+            audio.pause()
         }
     }
     let model = {
@@ -30,6 +42,15 @@
             let id = this.getSongId()
             this.model.get(id).then(()=>{
                 this.view.render(this.model.data)
+            })
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on('click','.play',()=>{
+                this.view.play()
+            })
+            $(this.view.el).on('click','.pause',()=>{
+                this.view.pause()
             })
         },
         getSongId() {
